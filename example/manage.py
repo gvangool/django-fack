424ace98d5c50566c232d4c41892c4f556a02e70
Gert Van Gool <gertvangool@gmail.com>
2019-05-03 23:15:31 +0200

    Update the example app based on Django 1.11
    Create a new project that can server as a example app. This fixes #12.

--- example/manage.py
+++ example/manage.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
-
 import os
 import sys
 
-try:
-    import faq
-except ImportError:
-    sys.stderr.write("django-fack isn't installed; trying to use a source checkout in ../fack.")
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
-
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
--- example/urls.py
+++ example/urls.py
@@ -1,21 +1,24 @@
-from django.conf.urls.defaults import patterns, url, include
-from django.contrib import admin; admin.autodiscover()
-from django.conf import settings
+"""example URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    # Just a simple example "home" page to show a bit of help/info.
-    url(r'^$', TemplateView.as_view(template_name="home.html")),
-    
-    # This is the URLconf line you'd put in a real app to include the FAQ views.
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^faq/', include('fack.urls')),
-    
-    # Everybody wants an admin to wind a piece of string around.
-    url(r'^admin/', include(admin.site.urls)),
-
-    # Normally we'd do this if DEBUG only, but this is just an example app.
-    url(regex  = r'^static/(?P<path>.*)$', 
-        view   = 'django.views.static.serve',
-        kwargs = {'document_root': settings.MEDIA_ROOT}
-    ),
-)
+    url(r'^$', TemplateView.as_view(template_name="home.html")),
+]

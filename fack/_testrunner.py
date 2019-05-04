3fde0f0c1204deeac05e0eca1725f92c9d8f9f77
Gert Van Gool <gertvangool@gmail.com>
2019-05-04 17:29:27 +0200

    Update the minimal set of SETTINGS for testrunner
    We update the minimal set of SETTINGS for testrunner by adding
    django.contrib.admin to INSTALLED_APPS and configuring the default
    TEMPLATES.

--- fack/_testrunner.py
+++ fack/_testrunner.py
@@ -11,6 +11,7 @@ settings.configure(
     DATABASES={"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory;"}},
     INSTALLED_APPS=[
         "django.contrib.auth",
+        "django.contrib.admin",
         "django.contrib.contenttypes",
         "django.contrib.messages",
         "django.contrib.sessions",
@@ -22,6 +23,21 @@ settings.configure(
         "django.middleware.csrf.CsrfViewMiddleware",
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
+    ],
+    TEMPLATES=[
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [],
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "context_processors": [
+                    "django.template.context_processors.debug",
+                    "django.template.context_processors.request",
+                    "django.contrib.auth.context_processors.auth",
+                    "django.contrib.messages.context_processors.messages",
+                ]
+            },
+        }
     ],
     ROOT_URLCONF="fack.urls",
 )
